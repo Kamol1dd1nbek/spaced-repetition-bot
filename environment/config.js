6d5ff238e6ffe3d3011ec55fdde5a46bac2ac2fe
Kamoliddinbek <kamoliddinbekxudoyberdi@gmail.com>
2024-08-31 12:30:09 +0500

    fixed a type checking to production

--- environment/config.js
+++ environment/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 
 const envFile =
-  process.env.NODE_ENV === "production" ? "production.env" : "development.env";
+  process.argv[2] === "production" ? "production.env" : "development.env";
 const result = dotenv.config({ path: `./environment/${envFile}` });
 
 if (result.error) {
