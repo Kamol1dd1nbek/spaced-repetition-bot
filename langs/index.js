c1115d68ae811fb433cdfc6bd8e70f0c2d19dcc9
Kamoliddinbek <kamoliddinbekxudoyberdi@gmail.com>
2024-12-16 14:33:32 +0500

    issues fixed about node version

--- langs/index.js
+++ langs/index.js
@@ -1,7 +1,13 @@
 import { context } from "../states/state.js";
-import uz from "./uz.json" assert { type: "json" };
-import en from "./en.json" assert { type: "json" };
-import ru from "./ru.json" assert { type: "json" };
+// import uz from "./uz.json" assert { type: "json" };
+// import en from "./en.json" assert { type: "json" };
+// import ru from "./ru.json" assert { type: "json" };
+
+import fs from "fs";
+
+const uz = JSON.parse(fs.readFileSync("./langs/uz.json", "utf-8"));
+const en = JSON.parse(fs.readFileSync("./langs/en.json", "utf-8"));
+const ru = JSON.parse(fs.readFileSync("./langs/ru.json", "utf-8"));
 
 let resources = {
   uz,
